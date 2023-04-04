e557a0d26cf31bac6155e85bf37fcf953247facb
Andrew Somers <avsomers25@gmail.com>
2023-04-04 13:36:35 -0400

    basic clustering

--- main.py
+++ main.py
@@ -15,25 +15,64 @@ template = cv2.imread('claw.png')
 filename = sct.shot()
 print(filename)  
 
-while True:
-    sct_img = sct.grab(bounding_box)
-    sct_img_np = np.array(sct_img)
+def find_cheese(img_rgb):
+    img_rgb = cv2.imread("test_1.png")[200:475, 0:1325]
 
+    lower_yellow = np.array([25, 150, 0])
+    upper_yellow = np.array([30, 255, 255])
+    mask = cv2.inRange(img_rgb, lower_yellow, upper_yellow)
+    res = cv2.bitwise_and(img_rgb, img_rgb, mask=mask)
+
+
+    points = cv2.findNonZero(mask)
+
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    # Find x,y coordinates of all non-white pixels in original image
+    Y, X = np.where(mask==255)
+    Z = np.column_stack((X,Y)).astype(np.float32)
+
+    nClusters = 2
+    ret,label,center=cv2.kmeans(Z,nClusters,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+
+    return center
+
+def find_claw(sct_img_np):
     find_claw_img = (sct_img_np[0:50, 0:1325]).astype(np.uint8)[:,:,:3]
     w, h = template.shape[:-1]
 
     res = cv2.matchTemplate(find_claw_img, template, cv2.TM_CCOEFF_NORMED)
     threshold = .8
     loc = np.where(res >= threshold)
-    for pt in zip(*loc[::-1]):  # Switch columns and rows
-        cv2.rectangle(sct_img_np, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
 
-    cv2.imshow('screen', sct_img_np)
+    try:
+        return sum(v[0] for v in loc) / float(len(loc))
+    except:
+        return 0 
+    
 
 
 
-    if (cv2.waitKey(1) & 0xFF) == ord('q'):
-        cv2.destroyAllWindows() 
-        break
+while True:
+    sct_img = sct.grab(bounding_box)
+
+    centers = find_cheese(sct_img)
+
+    sct_img_np = np.array(sct_img)
+
+    claw_loc = find_claw(sct_img_np)
+
+    for x,y in centers:
+        print("____")
+        print(x)
+        print(claw_loc)
+        print("____")
+        if(x == claw_loc):
+            print("OVER") 
+            break
 
 pyautogui.press('space')    
+
+
+
+
+
--- testcv.py
+++ testcv.py
@@ -2,16 +2,18 @@ import cv2
 import numpy as np
 
 
-img_rgb = cv2.imread("test_1.png")[0:50, 0:1325]
-
-
+img_rgb = cv2.imread("test_1.png")
+sct_img_np = np.array(img_rgb)
 template = cv2.imread('claw.png')
+
+find_claw_img = (sct_img_np[0:50, 0:1325]).astype(np.uint8)[:,:,:3]
 w, h = template.shape[:-1]
 
-res = cv2.matchTemplate(img_rgb, template, cv2.TM_CCOEFF_NORMED)
+res = cv2.matchTemplate(find_claw_img, template, cv2.TM_CCOEFF_NORMED)
 threshold = .8
 loc = np.where(res >= threshold)
+print(sum(v[0] for v in loc) / float(len(loc)))
 for pt in zip(*loc[::-1]):  # Switch columns and rows
-    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+    cv2.rectangle(sct_img_np, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
 
-cv2.imwrite('result.png', img_rgb)
+cv2.imwrite('result.png', sct_img_np)
